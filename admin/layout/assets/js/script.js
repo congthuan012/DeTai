b9b80a49c98acc2443e1b797ec085254310e8c8b
Huỳnh Công Thuần <76716043+congthuan012@users.noreply.github.com>
2021-12-12 07:51:09 +0700

    login layout

--- admin/layout/assets/js/script.js
+++ admin/layout/assets/js/script.js
@@ -1,3 +1,22 @@
-function PreviewImage(input,id){
-    document.getElementById(id).src = window.URL.createObjectURL(input.files[0])
+function PreviewImage(input, id) {
+  document.getElementById(id).src = window.URL.createObjectURL(input.files[0]);
 }
+
+//start show and hide password
+
+var input = document.getElementById("password");
+var hide = document.getElementById("password-hide");
+var show = document.getElementById("password-show");
+
+show.addEventListener("click", function () {
+  this.style.display = "none";
+  hide.style.display = "block";
+  input.type = "password";
+});
+
+hide.addEventListener("click", function () {
+  this.style.display = "none";
+  show.style.display = "block";
+  input.type = "text";
+});
+//end show and hide password
